a6d6bd4b18df310ea38cf7eee9db775f1cd790af
leprechaun <ac@steemfiles.com>
2021-06-19 19:52:20 -0300

    * number formatter works now with strings sent as well

--- src/common/util/formatted-number.ts
+++ src/common/util/formatted-number.ts
@@ -18,28 +18,40 @@ export default (value: number | string, options: Options | undefined = undefined
   }
 
   const { fractionDigits, prefix, suffix } = opts;
+  
+  let out : string = "";
+  if (typeof value == 'number') {	 	
+	  let format = "0,0";
+	
+	  if (fractionDigits) {
+		format += "." + "0".repeat(fractionDigits);
+	  }	
 
-  let format = "0,0";
-
-  if (fractionDigits) {
-    format += "." + "0".repeat(fractionDigits);
+	  out += numeral(value).format(format);
+	  //out = out.replace(',','');
+  } else  {
+  	  value = value.replace(',','');
+  	  const m = value.match(RegExp(/\d+(\.\d+)?/))
+  	  out = m ? m[0] : 'NaN';
   }
 
-  let out = "";
-
-  if (prefix) out += prefix + " ";
-  out += numeral(value).format(format);
   // add commas after the decimal point for readability
+  let decimal_location : number = out.length;
   for (let i = 0; i < out.length; ++i) {
   	  if (out[i] == '.') {
-  	  	  if (i+3 > out.length) {
-  	  	  	  break;
-  	  	  }
-  	  	  for (let j = i+4; j < out.length; j += 4) {
-  	  	  	  out = out.slice(0, j) + ',' + out.slice(j);
-  	  	  }
+  	  	  decimal_location = i;
   	  }
   }
+  if (decimal_location+3 <= out.length) {
+	  for (let j = decimal_location+4; j < out.length; j += 4) {
+		  out = out.slice(0, j) + ',' + out.slice(j);
+	  }
+  }
+  for (let j = decimal_location - 3; j >= 0; j -= 3) {  	  
+      if (j && out[j-1] != ',')
+  	    out = out.slice(0, j) + ',' + out.slice(j);
+  }
+  if (prefix) out = prefix + " " + out;
   if (suffix) out += " " + suffix;
 
   return out;
