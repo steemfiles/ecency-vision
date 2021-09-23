2e9e37742bade12df073e3b5dc69c5809fd99c3d
leprechaun <ac@steemfiles.com>
2021-09-23 20:33:08 -0300

    * do not display warning message if a post has been downvoted to zero Hive rewards

--- src/common/pages/entry.tsx
+++ src/common/pages/entry.tsx
@@ -383,9 +383,9 @@ class EntryPage extends BaseComponent<Props, State> {
     const { activeUser } = this.props;
     const ownEntry : boolean = (activeUser && activeUser.username === entry.author) || false;
     const tokenScotPost : ScotPost | null = (entry?.he && entry?.he[LIQUID_TOKEN_UPPERCASE]) || null;
-    const isHidden : boolean = (tokenScotPost && tokenScotPost.pending_token === 0 && tokenScotPost.active_votes.length > 0) || false;
+    const isHidden : boolean = (tokenScotPost && tokenScotPost.pending_token === 0 && tokenScotPost.total_payout_value === 0 && tokenScotPost.active_votes.length > 0) || false;
     // There is no list of muted user in neither the info or the config of the token...
-    const isMuted : boolean = (entry?.he && entry?.he[LIQUID_TOKEN_UPPERCASE].muted) || false;
+    const isMuted : boolean = (entry?.he && entry?.he[LIQUID_TOKEN_UPPERCASE] && entry?.he[LIQUID_TOKEN_UPPERCASE].muted) || false;
     const isLowReputation = false;
 
     //  Meta config
