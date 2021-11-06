59d02f2acc87e0145b16d5f572908a47c792a912
leprechaun <ac@steemfiles.com>
2021-11-06 10:44:29 -0300

    * syntax error fix

--- src/common/api/operations.ts
+++ src/common/api/operations.ts
@@ -357,7 +357,7 @@ export const transferHot = (
     },
   ];
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, () => {});
 };
@@ -545,7 +545,7 @@ export const transferToSavingsHot = (
     },
   ];
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, () => {});
 };
@@ -594,7 +594,7 @@ export const collateralizedConvertHot = (
     },
   ];
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   hs.sendOperation(op, params, () => {});
 };
@@ -634,7 +634,7 @@ export const convertHot = (owner: string, amount: string) => {
     },
   ];
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, () => {});
 };
@@ -685,7 +685,7 @@ export const transferFromSavingsHot = (
     },
   ];
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, () => {});
 };
@@ -850,7 +850,7 @@ export const delegateVestingSharesHot = (
   const currency = parts[parts.length - 1];
   const quantity = parts[0].replace(/,/g, "");
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, console.log);
 };
@@ -950,7 +950,7 @@ export const undelegateVestingSharesHot = (
     currency
   );
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, () => {});
 };
@@ -1040,7 +1040,7 @@ export const withdrawVestingHot = (
 ) => {
   const op: Operation = createWithdrawVestingOp(account, vestingShares, symbol);
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, fn);
 };
@@ -1083,7 +1083,7 @@ export const cancelWithdrawVesting = (
 export const cancelWithdrawVestingHot = (account: string, txID: string) => {
   const op: Operation = createCancelPowerDownOp(account, txID);
   const params: Parameters = {
-    callback: ${document.location.href,
+    callback: document.location.href
   };
   hs.sendOperation(op, params, () => {});
 };
@@ -1125,7 +1125,7 @@ export const setWithdrawVestingRouteHot = (
     },
   ];
   const params: Parameters = {
-    callback: document.location.href,
+    callback: document.location.href
   };
   return hs.sendOperation(op, params, () => {});
 };
