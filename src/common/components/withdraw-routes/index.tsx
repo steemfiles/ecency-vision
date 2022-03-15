6653dd6cb87c045c0c3a5c0fe0051b1dbfdc1db7
leprechaun <ac@steemfiles.com>
2022-03-15 17:23:50 -0300

    * added error boundary/ updated snapshot

--- src/common/components/withdraw-routes/index.tsx
+++ src/common/components/withdraw-routes/index.tsx
@@ -69,9 +69,9 @@ export class WithdrawRoutes extends BaseComponent<Props, State> {
   fetchRoutes = () => {
     const { activeUser } = this.props;
 
-    return getWithdrawRoutes(activeUser.username).then((routes) => {
-      this.stateSet({ routes });
-    });
+    return getWithdrawRoutes(activeUser.username)
+      .then((routes) => {})
+      .catch(console.error);
   };
 
   onInput = (
@@ -139,153 +139,163 @@ export class WithdrawRoutes extends BaseComponent<Props, State> {
   render() {
     const { routes, mode, inProgress, account, percent, auto } = this.state;
 
-    if (mode === "sign") {
+    try {
+      if (mode === "sign") {
+        return (
+          <>
+            {KeyOrHot({
+              ...this.props,
+              inProgress,
+              onKey: this.sign,
+              onHot: this.signHs,
+              onKc: this.signKS,
+            })}
+
+            <p className="text-center">
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.stateSet({ mode: "" });
+                }}
+                href="#"
+              >
+                {_t("g.back")}
+              </a>
+            </p>
+          </>
+        );
+      }
+
       return (
         <>
-          {KeyOrHot({
-            ...this.props,
-            inProgress,
-            onKey: this.sign,
-            onHot: this.signHs,
-            onKc: this.signKS,
-          })}
+          <Form
+            ref={this.form}
+            onSubmit={(e: React.FormEvent) => {
+              e.preventDefault();
+              e.stopPropagation();
 
-          <p className="text-center">
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                this.stateSet({ mode: "" });
-              }}
-              href="#"
-            >
-              {_t("g.back")}
-            </a>
-          </p>
+              if (!this.form.current?.checkValidity()) {
+                return;
+              }
+
+              this.stateSet({ mode: "sign" });
+            }}
+          >
+            <Form.Row>
+              <Form.Group as={Col}>
+                <Form.Label>{_t("withdraw-routes.account")}</Form.Label>
+                <Form.Control
+                  required={true}
+                  minLength={3}
+                  maxLength={20}
+                  autoComplete="off"
+                  autoCorrect="off"
+                  autoCapitalize="off"
+                  spellCheck="false"
+                  value={account}
+                  name="account"
+                  onChange={this.onInput}
+                  onInvalid={(e: any) =>
+                    handleInvalid(e, "withdraw-routes.", "validation-account")
+                  }
+                  onInput={handleOnInput}
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md={3}>
+                <Form.Label>{_t("withdraw-routes.percent")}</Form.Label>
+                <Form.Control
+                  type="number"
+                  min={0}
+                  max={100}
+                  required={true}
+                  value={percent}
+                  name="percent"
+                  onChange={this.onInput}
+                  onInvalid={(e: any) =>
+                    handleInvalid(e, "withdraw-routes.", "validation-percent")
+                  }
+                  onInput={handleOnInput}
+                />
+              </Form.Group>
+              <Form.Group as={Col} md={7}>
+                <Form.Label>{_t("withdraw-routes.auto-power-up")}</Form.Label>
+                <Form.Control
+                  as="select"
+                  value={auto}
+                  name="auto"
+                  onChange={this.onInput}
+                >
+                  <option value="yes">{_t("g.yes")}</option>
+                  <option value="no">{_t("g.no")}</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                md={2}
+                className="d-md-flex align-items-end justify-content-center"
+              >
+                <Button type="submit" size="sm">
+                  {_t("g.add")}
+                </Button>
+              </Form.Group>
+            </Form.Row>
+          </Form>
+
+          {routes.length > 0 && (
+            <table className="table route-table">
+              <thead>
+                <tr>
+                  <th>{_t("withdraw-routes.account")}</th>
+                  <th>{_t("withdraw-routes.percent")}</th>
+                  <th>{_t("withdraw-routes.auto-power-up")}</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {routes.map((r) => {
+                  try {
+                    return (
+                      <tr key={r.id}>
+                        <td>{r.to_account}</td>
+                        <td>{`${r.percent / 100}%`}</td>
+                        <td>{r.auto_vest ? _t("g.yes") : _t("g.no")}</td>
+                        <td>
+                          <Tooltip content={_t("g.delete")}>
+                            <a
+                              href="#"
+                              onClick={(e) => {
+                                e.preventDefault();
+
+                                this.stateSet({
+                                  account: r.to_account,
+                                  percent: "0",
+                                  auto: "no",
+                                  mode: "sign",
+                                });
+                              }}
+                            >
+                              {deleteForeverSvg}
+                            </a>
+                          </Tooltip>
+                        </td>
+                      </tr>
+                    );
+                  } catch (e) {
+                    return <span>Error: {e.message}</span>;
+                  }
+                })}
+              </tbody>
+            </table>
+          )}
         </>
       );
+    } catch (e) {
+      console.error(e.message);
+      console.log(e);
+      return null;
     }
-
-    return (
-      <>
-        <Form
-          ref={this.form}
-          onSubmit={(e: React.FormEvent) => {
-            e.preventDefault();
-            e.stopPropagation();
-
-            if (!this.form.current?.checkValidity()) {
-              return;
-            }
-
-            this.stateSet({ mode: "sign" });
-          }}
-        >
-          <Form.Row>
-            <Form.Group as={Col}>
-              <Form.Label>{_t("withdraw-routes.account")}</Form.Label>
-              <Form.Control
-                required={true}
-                minLength={3}
-                maxLength={20}
-                autoComplete="off"
-                autoCorrect="off"
-                autoCapitalize="off"
-                spellCheck="false"
-                value={account}
-                name="account"
-                onChange={this.onInput}
-                onInvalid={(e: any) =>
-                  handleInvalid(e, "withdraw-routes.", "validation-account")
-                }
-                onInput={handleOnInput}
-              />
-            </Form.Group>
-          </Form.Row>
-          <Form.Row>
-            <Form.Group as={Col} md={3}>
-              <Form.Label>{_t("withdraw-routes.percent")}</Form.Label>
-              <Form.Control
-                type="number"
-                min={0}
-                max={100}
-                required={true}
-                value={percent}
-                name="percent"
-                onChange={this.onInput}
-                onInvalid={(e: any) =>
-                  handleInvalid(e, "withdraw-routes.", "validation-percent")
-                }
-                onInput={handleOnInput}
-              />
-            </Form.Group>
-            <Form.Group as={Col} md={7}>
-              <Form.Label>{_t("withdraw-routes.auto-power-up")}</Form.Label>
-              <Form.Control
-                as="select"
-                value={auto}
-                name="auto"
-                onChange={this.onInput}
-              >
-                <option value="yes">{_t("g.yes")}</option>
-                <option value="no">{_t("g.no")}</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group
-              as={Col}
-              md={2}
-              className="d-md-flex align-items-end justify-content-center"
-            >
-              <Button type="submit" size="sm">
-                {_t("g.add")}
-              </Button>
-            </Form.Group>
-          </Form.Row>
-        </Form>
-
-        {routes.length > 0 && (
-          <table className="table route-table">
-            <thead>
-              <tr>
-                <th>{_t("withdraw-routes.account")}</th>
-                <th>{_t("withdraw-routes.percent")}</th>
-                <th>{_t("withdraw-routes.auto-power-up")}</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {routes.map((r) => {
-                return (
-                  <tr key={r.id}>
-                    <td>{r.to_account}</td>
-                    <td>{`${r.percent / 100}%`}</td>
-                    <td>{r.auto_vest ? _t("g.yes") : _t("g.no")}</td>
-                    <td>
-                      <Tooltip content={_t("g.delete")}>
-                        <a
-                          href="#"
-                          onClick={(e) => {
-                            e.preventDefault();
-
-                            this.stateSet({
-                              account: r.to_account,
-                              percent: "0",
-                              auto: "no",
-                              mode: "sign",
-                            });
-                          }}
-                        >
-                          {deleteForeverSvg}
-                        </a>
-                      </Tooltip>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
-      </>
-    );
   }
 }
 
